b0282dff59d25865d825e3d13b8505e52992184a
ShinDongho <namomo@gmail.com>
2022-01-04 17:57:46 +0900

    react-coding-apple 애플코딩 리액트 - 쇼핑몰
    * feat: Link 기능을 통해 Router 페이지 이동
    * feat: history 로 이전 페이지 이동
    * feat: 변경전 코드들 삭제
    * feat: styled component
    * feat: sass 코드들

--- react-coding-apple/shop/src/App.js
+++ react-coding-apple/shop/src/App.js
@@ -20,15 +20,15 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
+              <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
+              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -36,78 +36,41 @@ function App() {
 
       
 
-
       {/* 라우팅 - exact 를 추가해 지정된 내용만 보여줌 */}
-      <Route exact path="/">
-        <div>메인 페이지</div>
-        {/* Jumbotron 대신 */}
-        <div className="background">
-          <h1>20% Season Off</h1>
-          <p>
-            Nullam quis risus eget urna mollis ornare vel eu leo. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
-          </p>
-          <p>
-            <button variant="primary">Lean more</button>
-          </p>
-        </div>
-      </Route>
-      
+      <Switch>
 
-      <Route path="/detail">
+        <Route path="/detail/:id">
+          <div>상세 페이지</div>
+          <Detail shoes={shoes}></Detail>
+        </Route>
 
-        <Detail></Detail>
-        {/* <div>상세 페이지</div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
-            </div>
+        <Route path="/">
+          <div>메인 페이지</div>
+          {/* Jumbotron 대신 */}
+          <div className="background">
+            <h1>20% Season Off</h1>
+            <p>
+              Nullam quis risus eget urna mollis ornare vel eu leo. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.
+            </p>
+            <p>
+              <button variant="primary">Lean more</button>
+            </p>
           </div>
-        </div>  */}
-      </Route>
 
-
-      {/* 공통 */}
-      <div className="container">
-        <div className="row">
-
-          {/* {
+          {
             shoes.map((e, idx) => {
               return (
                 <Card imgIdx={idx} shoes={e} key={idx}></Card>
-                // <Product imgIdx={idx} title={e.title} content={e.content} price={e.price}></Product>
               )
             })
-          } */}
+          }
+        </Route>
 
-          {/* <Product title={shoes[0].title} content={shoes[0].content} price={shoes[0].price}></Product>
-          <Product title={shoes[1].title} content={shoes[1].content} price={shoes[1].price}></Product>
-          <Product title={shoes[2].title} content={shoes[2].content} price={shoes[2].price}></Product> */}
-
-          {/* <div className="com-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-              <h4> { shoes[0].title } </h4>
-              <p>상품설명 & 가격</p>
-          </div>
-          <div className="com-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
-              <h4>상품명</h4>
-              <p>상품설명 & 가격</p>
-          </div>
-          <div className="com-md-4">
-          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
-              <h4>상품명</h4>
-              <p>상품설명 & 가격</p>
-          </div> */}
-
-        </div>
-      </div>
+        <Route path="/:id">
+          <div>id 들어갈때</div>
+        </Route>
+        
+      </Switch>
 
     </div>
   );
@@ -118,7 +81,7 @@ function Card(props) {
   let img = `https://codingapple1.github.io/shop/shoes${props.imgIdx+1}.jpg`;
   return (
     <div className="col-md-4">
-      <img src={ img } width="100%" />
+      <img src={ img } alt="shoes" width="100%" />
         <h4>{ props.shoes.title }</h4>
         <p>{ props.shoes.content } & { props.shoes.price }</p>
     </div>
@@ -129,7 +92,7 @@ function Product(props) {
   let img = `https://codingapple1.github.io/shop/shoes${props.imgIdx+1}.jpg`;
   return (
     <div className="col-md-4">
-      <img src={ img } width="100%" />
+      <img src={ img } alt="shoes" width="100%" />
         <h4>{ props.title }</h4>
         <p>{ props.content } & { props.price }</p>
     </div>
--- react-coding-apple/shop/src/Detail.js
+++ react-coding-apple/shop/src/Detail.js
@@ -1,21 +1,58 @@
 import { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
 
-function Detail () {
-    return (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-            </div>
-            <div className="col-md-6 mt-4">
-              <h4 className="pt-5">상품명</h4>
-              <p>상품설명</p>
-              <p>120000원</p>
-              <button className="btn btn-danger">주문하기</button> 
-            </div>
-          </div>
-        </div> 
-    )
+let CompBox = styled.div`
+  padding : 20px;
+`;
+let CompH4 = styled.h4`
+  font-size: 15px;
+  color: ${ props => props.hcolor };
+`;
+
+function Detail (props) {
+  let history = useHistory();
+  let { id } = useParams();
+
+  let item = props.shoes.find((e) => {
+    // console.log(`shoes id[${e.id}] === parameter id[${id}]`);
+    if (e.id == id) return true;
+  });
+
+  return (
+    <div className="container">
+
+      <CompBox>
+        <CompH4 hcolor={ 'red' }>상품에 관하여</CompH4>
+      </CompBox>
+
+      <div className='my-alert'>
+        <p>마감 임박</p>
+      </div>
+      <div className='my-alert2'>
+        <p>마감 임박 - 2</p>
+      </div>
+      <div className='my-alert3'>
+        <p>마감 임박 - 3</p>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" alt="shoes" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">상품명 : { item.title }</h4>
+          <p className='red'> { item.content } </p>
+          <p> { item.price } </p>
+          <button className="btn btn-danger">주문하기</button> 
+          <button className="btn btn-danger" onClick={ () => {
+            history.goBack();      
+          }}>뒤로가기</button> 
+        </div>
+      </div>
+    </div> 
+  )
 }
 
 export default Detail;
