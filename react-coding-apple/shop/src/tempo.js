f40fa3b00fde3dbdea8ce62a59acd2dd11f68964
ShinDongho <namomo@gmail.com>
2022-01-16 16:40:07 +0900

    react-coding-apple 애플코딩 리액트 - 쇼핑몰
    * feat: useEffect 를 통해 변동값 모니터링 방법 추가

--- react-coding-apple/shop/src/tempo.js
+++ react-coding-apple/shop/src/tempo.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Tempo() {
   let [count, setCount] = useState(0);
   let [age, setAge] = useState(20);
+
+  // 등록된 값이 변동되면 실행
+  useEffect(() => {
+    if (count < 3) {
+      setAge(age+1);
+    }
+  }, [count]);
 
   return (
     <div>
       <div>안녕하십니까 전 {age}</div>
       <div>카운트는 {count}</div>
       <button onClick={() => {
-        if (count < 3) {
-          setAge(age+1);
-        }
+        // if (count < 3) {
+        //   setAge(age+1);
+        // }
         setCount(count+1);
         
       }}>누르면한살먹기</button>
